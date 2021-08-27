db31e5667b2aa20215d8af4da117b46a7231e2d1
Alexandra Moore <alexmoore94@hotmail.co.uk>
2021-08-27 23:05:16 +0100

    Protect endpoints and verify Auth0 JWT

--- src/savings/savings.module.ts
+++ src/savings/savings.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { SavingsService } from './savings.service';
 import { SavingsController } from './savings.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { SavingsSchema } from './schemas/savings.schema';
+import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
 
 @Module({
   imports: [
@@ -11,4 +12,13 @@ import { SavingsSchema } from './schemas/savings.schema';
   providers: [SavingsService],
   controllers: [SavingsController]
 })
-export class SavingsModule {}
+
+export class SavingsModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+    consumer.apply(AuthenticationMiddleware).forRoutes(
+      { method: RequestMethod.POST, path: '/savings/account' },
+      { method: RequestMethod.PUT, path: '/savings/edit' },
+      { method: RequestMethod.DELETE, path: '/savings/delete' }
+    )
+  }
+}
